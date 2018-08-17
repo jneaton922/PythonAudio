ca5157056a64b6e47c70bd2aa2d52a2060ee7d10
Josh Eaton <jneaton922@gmail.com>
2018-08-17 13:46:33 -0400

    Separated Mic polling to its own module

--- AudioPlots.py
+++ AudioPlots.py
@@ -1,27 +1,22 @@
 import MicrophonePoll
-from multiprocessing import Process,Queue
 import numpy as np
 from Tkinter import *
 from struct import unpack
-import time
-
-
-
 
 
 def calculate_levels(data):
-   # Convert raw data to numpy array
-    data = unpack("%dh"%((len(data)/2)),data)
+    #  Convert raw data to numpy array
+    data = unpack("%dh" % ((len(data)/2)), data)
     data = np.array(data, dtype='h')
-   # Apply FFT - real data so rfft used
-    fourier=np.fft.rfft(data)
-   # Remove last element in array to make it the same size as chunk
-    fourier=np.delete(fourier,len(fourier)-1)
-   # Find amplitude
+    # Apply FFT - real data so rfft used
+    fourier = np.fft.rfft(data)
+    #  Remove last element in array to make it the same size as chunk
+    fourier = np.delete(fourier, len(fourier)-1)
+    #  Find amplitude
     power = (np.log10(np.abs(fourier))**2)-5
-    for i in range (0,len(fourier)):
-    #for i in range (4,50):
-        for stars in range(0,int(power[i])):
+
+    for i in range(0, len(fourier)):
+        for stars in range(0, int(power[i])):
                 print "*",
         print ""
     print "End"
@@ -29,16 +24,12 @@ def calculate_levels(data):
 
 
 def main():
-    mic = MicrophonePoll.Microphone()
-    dataQueue=Queue()
-    mic_process = Process(target=mic.listen,args=(dataQueue,))
-    mic_process.start()
+    mic = MicrophonePoll.Microphone(sr=8000, fps=160)
+    mic.setup()
 
     while True:
-        if not dataQueue.empty():
-            calculate_levels(dataQueue.get_nowait())
-        else:
-            time.sleep(.001)
+        # print "LOOP"
+        calculate_levels(mic.get_data()[1])
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -14,80 +14,77 @@ matplotlib.use('TkAgg')
 # Set up audio
 sample_rate = 32000
 no_channels = 1
-chunk =  1280# Use a multiple of 8
-waterfallframes=200
+chunk = 1280  # Use a multiple of 8
+waterfallframes = 200
 data_in = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NORMAL)
 data_in.setchannels(no_channels)
 data_in.setrate(sample_rate)
 data_in.setformat(aa.PCM_FORMAT_S16_LE)
 data_in.setperiodsize(chunk)
 
+
 def calculate_levels(data):
-   # Convert raw data to numpy array
-    data = unpack("%dh"%((len(data)/2)),data)
+    #  Convert raw data to numpy array
+    data = unpack("%dh" % ((len(data)/2)), data)
     data = np.array(data, dtype='h')
-   # Apply FFT - real data so rfft used
-    fourier=np.fft.rfft(data)
-   # Remove last element in array to make it the same size as chunk
-    fourier=np.delete(fourier,len(fourier)-1)
-   # Find amplitude
+    # Apply FFT - real data so rfft used
+    fourier = np.fft.rfft(data)
+    # Remove last element in array to make it the same size as chunk
+    fourier = np.delete(fourier, len(fourier)-1)
+#   Find amplitude
     power = (np.log10(np.abs(fourier))**2)-5
     return power
 
 
-
 root = Tk()
 
-#mainframe = PanedWindow(root,orient=VERTICAL,showhandle=True,bg='black')
+#  mainframe = PanedWindow(root,orient=VERTICAL,showhandle=True,bg='black')
 
-
-wftFrame=Frame(root)
+wftFrame = Frame(root)
 
 fig = plt.figure()
 plot = fig.add_subplot(111)
 
-xdata = np.linspace(0,chunk/2,chunk/2)
-ydata = np.zeros(chunk/2,dtype=float)
-waterfalldata=np.zeros([chunk/2,waterfallframes],dtype=float)
+xdata = np.linspace(0, chunk/2, chunk/2)
+ydata = np.zeros(chunk/2, dtype=float)
+waterfalldata = np.zeros([chunk/2, waterfallframes], dtype=float)
 
-line, = plot.plot(xdata,ydata,'-',color='green')
-plot.set_ylim(0,waterfallframes-1)
+line, = plot.plot(xdata, ydata, '-', color='green')
+plot.set_ylim(0, waterfallframes-1)
 
-canvas = FigureCanvasTkAgg(fig,master=wftFrame)
-plotwidg=canvas.get_tk_widget()
+canvas = FigureCanvasTkAgg(fig, master=wftFrame)
+plotwidg = canvas.get_tk_widget()
 
+wfplot = fig.add_subplot(111)
+waterfalldata = np.zeros([waterfallframes, chunk/2], dtype=float)
+im = wfplot.imshow(waterfalldata, interpolation='nearest', origin='bottom',
+                    aspect='auto', vmin=-10, vmax=20, cmap='bone')
 
+wfcanvas = FigureCanvasTkAgg(fig, master=wftFrame)
+wfwidg = wfcanvas.get_tk_widget()
+wfwidg.pack(side=TOP, fill=BOTH, expand=True)
 
-wfplot=fig.add_subplot(111)
-waterfalldata=np.zeros([waterfallframes,chunk/2],dtype=float)
-im = wfplot.imshow(waterfalldata,interpolation='nearest',origin='bottom',aspect='auto',vmin=-10,vmax=20,cmap='bone')
-wfcanvas=FigureCanvasTkAgg(fig,master=wftFrame)
-wfwidg=wfcanvas.get_tk_widget()
-wfwidg.pack(side=TOP,fill=BOTH,expand=True)
-
-
-wftFrame.pack(side=TOP,fill=BOTH,expand=True)
+wftFrame.pack(side=TOP, fill=BOTH, expand=True)
 
 root.title("Audio Plots")
-wrap=False
-timer=waterfallframes
+wrap = False
+timer = waterfallframes
 while True:
-   timer-=1
-   # Read data
-   data_in.setperiodsize(chunk)
-   l,data = data_in.read()
-   power = calculate_levels(data)
-   if not wrap:
-       waterfalldata[timer]=power
-   else:
-       waterfalldata=np.roll(waterfalldata,(waterfallframes-2)*len(power))
-       waterfalldata[waterfallframes-1]=power
-   line.set_data(xdata,(power*(waterfallframes/100))+(waterfallframes/20))
-   im.set_data(waterfalldata)
-   fig.canvas.draw()
-   if timer==0:
-       wrap=True
-       timer=waterfallframes-1
-
+    timer -= 1
+    # Read data
+    data_in.setperiodsize(chunk)
+    l, data = data_in.read()
+    power = calculate_levels(data)
+    if not wrap:
+        waterfalldata[timer] = power
+    else:
+        waterfalldata = np.roll(waterfalldata, len(power))
+        waterfalldata[0] = power
+    line.set_data(xdata, (power*(waterfallframes/100))+(waterfallframes/20))
+    im.set_data(waterfalldata)
+    fig.canvas.draw()
+    if timer == 0:
+        wrap = True
+        timer = waterfallframes-1
 
 root.mainloop()
